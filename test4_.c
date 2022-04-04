bf43b58a3ffc8ebbdd837ec7c476b5ae03048916
shixiangli123 <102905417+shixiangli123@users.noreply.github.com>
2022-04-04 22:55:06 +0800

    Add files via upload

---
+++ test4_.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int time = 0;
+	int m, n;
+	scanf("%d%d", &m, &n);
+	printf("%d %d\n", m, n);
+	int* a;
+	a = (int*)malloc(sizeof(int) * m);
+	int i,num=n-m,en=0;//num是最开始没占到取件机的人
+	for (i = 0; i < m; i++)
+	{
+		scanf("%d", &a[i]);	
+	}
+	while (1)
+	{
+		for (i = 0; i < m; i++)
+		{
+			a[i]--;
+			if (a[i] == 0 && num > 0)
+			{
+				scanf("%d", &a[i]);
+				num--;//前面空出的位置由后面排队的人接上，人数少1
+			}
+			if (a[i] ==0)
+				en++;//有一个机子彻底没人排了
+		}//一秒过去
+		time++;
+		if (en == m)
+			break;
+	}
+	printf("%d\n", time);
+	free(a);
+	return 0;
+}
