197a5c06db0cf19aacb9f61c743eef6742755a71
shixiangli123 <102905417+shixiangli123@users.noreply.github.com>
2022-04-05 18:33:42 +0800

    Update test3_.c

--- test3_.c
+++ test3_.c
@@ -1,49 +1,56 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-int jisuan(int n)
+int jisuan(int n)//函数作用：计算最少步数，使用递归思想，只考虑下一步，是否除2
 {
-
-
+	if(n==1)
+		return 0;
+	if (n % 2 == 0)
+	{
+		return 1 + jisuan(n / 2);
+	}
+	int x = jisuan(n + 1);
+	int y = jisuan(n - 1);
+	if (x > y)
+	{
+		return y + 1;
+	}
+	else
+	{
+		return x + 1;
+	}
+}
+int proce(int n,int g)//函数作用：这里主要负责打印运算符
+{
+	if (n == 1)
+		return 0;
+	if (n % 2 == 0)
+	{
+		if(g==1)//g用来判断是否打印
+			printf("^");
+		
+		return (1 + proce(n / 2,g));
+	}
+	int x = proce(n + 1,0);//不打印
+	int y = proce(n - 1,0);
+	if (x > y)
+	{
+		if(g==1)
+		printf("-");
+		
+		return proce(n - 1, g) + 1;
+	}
+	else 
+	{
+		if(g==1)
+		printf("+");
+		return  proce(n + 1, g) + 1;
+	}
 }
 int main()
 {
-	int n,count=0;
+	int n,count;
 	scanf("%d", &n);
-	int m = n;
-	while(1)
-	{
-		if (n % 2 == 0)
-		{
-			n = n / 2;
-			count++;
-		}
-		else
-		{
-			n = n - 1;
-			count++;
-		}
-		if (n == 1)
-			break;
-	}
+	count=jisuan(n);
 	printf("%d\n", count);
-	while (1)
-	{
-		if (m % 2 == 0)
-		{
-			m = m / 2;
-			printf("^");
-		}
-		else
-		{
-			m = m - 1;
-			printf("-");
-		}
-		if (m == 1)
-			break;
-	}
-
-
-
-
-
+	proce(n,1);
 }
